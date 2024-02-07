d7c08b422e0b948f1d722d1ffe696fa845556ac4
Oli Hock <oliver.hock@gmail.com>
2024-02-07 18:17:27 +0100

    Initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,31 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Auth0Provider} from "@auth0/auth0-react";
+import LoginButton from "./login";
+import LogoutButton from "./logout";
+import Profile from "./profile";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
+
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <Auth0Provider
+            domain="dev-gpha7v2j2cxhneb2.us.auth0.com"
+            clientId="tOJiOnVnMAk1TwQVFfcGMZzWdzATso6E"
+            authorizationParams={{
+                redirect_uri: window.location.origin
+            }}
+        >
+            <LoginButton />
+            <LogoutButton />
+            <br />
+            <Profile />
+        </Auth0Provider>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/profile.tsx
@@ -0,0 +1,28 @@
+import { useAuth0 } from "@auth0/auth0-react";
+import React from "react";
+
+const Profile = () => {
+    const { user, isAuthenticated, isLoading } = useAuth0();
+
+    if (isLoading) {
+        return <div>Loading ...</div>;
+    }
+
+    if(!isAuthenticated) {
+        return <div>Not authenticated</div>;
+    }
+
+    if(user == undefined) {
+        return <div>user is undefined</div>;
+    }
+
+    return (
+            <div>
+                <img src={user.picture} alt={user.name} />
+                <h2>{user.name}</h2>
+                <p>{user.email}</p>
+            </div>
+    );
+};
+
+export default Profile;
